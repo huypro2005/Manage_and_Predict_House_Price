c8429f3d9bdd498e1d5ff9e26a8946a92be5d567
Cao Thành Huy <145009627+huypro2005@users.noreply.github.com>
2025-09-28 00:05:14 +0700

    aa

--- backendWeb/backendWeb/settings.py
+++ backendWeb/backendWeb/settings.py
@@ -116,6 +116,8 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
+        "CONN_MAX_AGE": 6,
+        
     }
 }
 
